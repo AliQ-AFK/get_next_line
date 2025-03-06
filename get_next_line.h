c9d3ccde1e50548c6ddfb2d8d1ea31fb48218c3c
Ali Qeblawi <aliiqeblawii@gmail.com>
2025-03-06 01:35:02 +0100

    included the newly created functions

--- get_next_line.h
+++ get_next_line.h
@@ -11,20 +11,19 @@
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-# include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-char	*get_next_line(int fd);
+#include <stdlib.h>
+#include <unistd.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
 size_t	ft_strlen(const char *str);
-char	*ft_strdup(const char *src);
-char	*ft_substr(char const *s, size_t start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(char const *src);
+size_t	ft_strlcpy(char *dest, const char *src, size_t size);
+char	*ft_substr(char const *s, size_t start, size_t len);
 char	*ft_strchr(const char *s, int c);
-void	*ft_calloc(size_t count, size_t size);
+char *get_next_line(int fd);
+
 #endif
